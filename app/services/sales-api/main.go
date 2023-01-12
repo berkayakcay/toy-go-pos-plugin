0fda5a6571c1d6ecad5887c29b3537ef930ff115
Berkay Akçay <berkayakcay@outlook.com>
2023-01-12 17:34:23 +0300

    2.3

--- app/services/sales-api/main.go
+++ app/services/sales-api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/berkayakcay/toy-pos-plugin/foundation/logger"
+	"go.uber.org/automaxprocs/maxprocs"
 	"go.uber.org/zap"
 	"os"
 	"os/signal"
@@ -33,6 +34,19 @@ func main() {
 }
 
 func run(log *zap.SugaredLogger) error {
+
+	// =========================================================================
+	// GOMAXPROCS
+
+	// Want to see what maxprocs reports.
+	opt := maxprocs.Logger(log.Infof)
+
+	// Set the correct number of threads for the service
+	// based on what is available either by the machine or quotas.
+	if _, err := maxprocs.Set(opt); err != nil {
+		return fmt.Errorf("maxprocs: %w", err)
+	}
+
 	log.Infow("startup", "GOMAXPROCS", runtime.GOMAXPROCS(0), "build", build)
 
 	// Make a channel to listen for an interrupt or terminate signal from the OS.
--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/berkayakcay/toy-pos-plugin
 
 go 1.19
 
-require go.uber.org/zap v1.24.0
+require (
+	go.uber.org/automaxprocs v1.5.1
+	go.uber.org/zap v1.24.0
+)
 
 require (
 	go.uber.org/atomic v1.10.0 // indirect
